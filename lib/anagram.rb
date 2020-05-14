2e9c3e680dc73ec8ed06669d368f1f3fa088e90d
kiku ishimoto <kikuish@gmail.com>
2020-05-14 16:35:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,20 @@
 class Anagram
-  
+  attr_accessor word:
+  def initialize(word)
+    @word = word
+  end
+  def match(array)
+    nuarray = []
+    empty =[]
+    array.each do |words|
+      if words.split('').sort == @word.split('').sort
+        nuarray << words 
+      end 
+    end 
+    return empty if nuarray.empty?
+  end
+        
+        
+
 end
+
